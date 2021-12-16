0a97f1c64fce8cf739a8036daa9b894c986fe092
cyberowl <cybrowl@protonmail.com>
2021-12-15 18:04:43 -0800

    add hammerspoon commands

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,19 @@
-
 hs.hotkey.bind({"ctrl"}, "3", function()
   hs.application.launchOrFocus("Alacritty")
 end)
 
 hs.hotkey.bind({"ctrl"}, "2", function()
   hs.application.launchOrFocus("Brave Browser")
+end)
+
+hs.hotkey.bind({"ctrl"}, "q", function()
+  hs.application.launchOrFocus("Quiver")
+end)
+
+hs.hotkey.bind({"ctrl"}, "l", function()
+  hs.application.launchOrFocus("GitKraken")
+end)
+
+hs.hotkey.bind({"ctrl"}, "c", function()
+  hs.application.launchOrFocus("Visual Studio Code")
 end)
